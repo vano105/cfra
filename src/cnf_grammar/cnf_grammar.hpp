4c2ae14d38240550c33f28d73c21ffd26a3fadd8
vano105 <ivankhromov05@mail.ru>
2024-11-09 16:21:10 +0300

    add cnf grammar download from file constructor

--- src/cnf_grammar/cnf_grammar.hpp
+++ src/cnf_grammar/cnf_grammar.hpp
@@ -1,6 +1,10 @@
 #pragma once
+#include <filesystem>
+#include <fstream>
 #include <functional>
+#include <iostream>
 #include <set>
+#include <stdexcept>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -26,9 +30,7 @@ public:
 
     bool operator<(const symbol &other) const { return label_ < other.label_; }
 
-    operator std::string() const {
-      return label_;
-    }
+    operator std::string() const { return label_; }
 
     int hash() { return std::hash<std::string>{}(label_); }
   };
@@ -53,7 +55,61 @@ public:
       : start_nonterm_(start_nonterm), epsilon_rules_(epsilon_rules),
         simple_rules_(simple_rules), complex_rules_(complex_rules) {}
 
-  cnf_grammar(const std::string &path) {}
+  cnf_grammar(const std::string &path) {
+    std::ifstream infile(path);
+    std::string line;
+    auto strip = [](auto str) {
+      std::string new_str;
+
+      for (auto symb : str)
+        if (symb == ' ')
+          new_str.push_back(symb);
+      return new_str;
+    };
+    auto split = [](auto str, std::string sep) {
+      std::vector<std::string> result;
+      size_t pos = 0;
+      std::string token;
+
+      while ((pos = str.find(sep)) != std::string::npos) {
+        token = sep.substr(0, pos);
+        result.push_back(token);
+        str.erase(0, pos + sep.length());
+      }
+      result.push_back(str);
+
+      return result;
+    };
+
+    if (infile.is_open()) {
+      if (std::getline(infile, line)) {
+        this->start_nonterm_ = line.substr(0, line.find(' '));
+      }
+
+      std::getline(infile, line);
+      while (std::getline(infile, line)) {
+        std::string lhs = strip(line.substr(0, line.find("->")));
+        std::string rhs = strip(line.substr(line.find("->") + 2));
+        std::vector<std::string> rhs_parts = split(rhs, " ");
+        if (rhs_parts.size() == 1) {
+          epsilon_rules_.push_back(symbol(rhs_parts[0]));
+        } else if (rhs_parts.size() == 2) {
+          simple_rules_.push_back(
+              std::tuple(symbol(rhs_parts[0]), symbol(rhs_parts[1])));
+        } else if (rhs_parts.size() == 2) {
+          complex_rules_.push_back(std::tuple(symbol(rhs_parts[0]),
+                                              symbol(rhs_parts[1]),
+                                              symbol(rhs_parts[2])));
+        } else {
+          std::runtime_error("error in cnf file format");
+        }
+      }
+
+      infile.close();
+    } else {
+      std::cerr << "Unable to open file: " << path << std::endl;
+    }
+  }
 
   cnf_grammar &operator=(const cnf_grammar &other) = default;
 
