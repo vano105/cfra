a550ec4a332a14d7c6330fb2ee57dc8f793212ed
vano105 <ivankhromov05@mail.ru>
2024-11-11 00:59:24 +0300

    Add load label graph from txt file

--- src/label_decomposed_graph/label_decomposed_graph.hpp
+++ src/label_decomposed_graph/label_decomposed_graph.hpp
@@ -1,13 +1,13 @@
 #pragma once
+#include <cstddef>
 #include <cubool.h>
+#include <fstream>
+#include <iostream>
 #include <map>
+#include <sstream>
 #include <string>
+#include <utility>
 #include <vector>
-
-/*struct cuBool_Matrix {
-  std::vector<std::vector<std::string>> m;
-  cuBool_Matrix &ewiseadd(const cuBool_Matrix &matr) { return *this; }
-};*/
 
 class label_decomposed_graph {
 private:
@@ -20,7 +20,48 @@ public:
 
   label_decomposed_graph(const size_t size) : matrix_size(size) {}
 
-  label_decomposed_graph(const std::string &file) {}
+  label_decomposed_graph(const std::string &path) {
+    std::ifstream file(path);
+    std::map<std::string, std::pair<std::vector<int>, std::vector<int>>>
+        result_matrix;
+
+    if (!file.is_open()) {
+      std::cerr << "Can't open file: " << path << std::endl;
+    }
+    std::string line;
+    size_t line_count = 0;
+    while (std::getline(file, line)) {
+      line_count++;
+      std::istringstream iss(line);
+      size_t v, to;
+      std::string label;
+
+      if (!(iss >> v >> label >> to)) {
+        std::cerr << "Wrong file format: " << line << std::endl;
+        continue;
+      }
+      matrix_size = std::max(std::max(matrix_size, v), to);
+      result_matrix[label].first.emplace_back(v);
+      result_matrix[label].first.emplace_back(to);
+    }
+
+    for (auto &it : result_matrix) {
+      matrices[it.first] = nullptr;
+      cuBool_Matrix_New(&matrices[it.first], matrix_size, matrix_size);
+      size_t number_of_cols = result_matrix[it.first].first.size();
+      cuBool_Index *rows = new cuBool_Index(number_of_cols);
+      cuBool_Index *cols = new cuBool_Index(number_of_cols);
+      size_t i = 0;
+      for (auto x : result_matrix[it.first].first)
+        rows[++i];
+      i = 0;
+      for (auto x : result_matrix[it.first].second)
+        cols[++i];
+      cuBool_Matrix_Build(matrices[it.first], rows, cols, number_of_cols,
+                          CUBOOL_HINT_NO);
+    }
+    file.close();
+  }
 
   label_decomposed_graph(const label_decomposed_graph &other)
       : matrices(other.matrices) {}
